5a8a8a956532ba5a8b46af9de01b6cf39b19ec74
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-06-12 15:07:06 +0000

    FIX: Remove model cod page as redundant

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from app_pages.multipage import MultiPage
 
 # Import your pages
 from app_pages.page_summary import page_summary_body
-from app_pages.page_model_cod import page_model_cod_body
 from app_pages.page_predict import page_predict_body
 from app_pages.page_cod_analysis import page_cod_analysis_body
 from app_pages.page_cod_hypotheses import page_cod_hypotheses_body
@@ -13,7 +12,6 @@ app = MultiPage(app_name="Nod to the COD")
 
 # Register pages
 app.add_page("Project Summary", page_summary_body)
-app.add_page("Model Insights", page_model_cod_body)
 app.add_page("Predict COD", page_predict_body)
 app.add_page("COD Analysis Explorer", page_cod_analysis_body)
 app.add_page("Project Hypotheses", page_cod_hypotheses_body)
